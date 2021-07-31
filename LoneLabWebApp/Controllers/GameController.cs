46e09bb67e121af3697bdbee681bef7fc71047fe
Kirill Shevchenko <kirillwowlol@yandex.ru>
2021-07-31 12:25:09 +0200

    Add service and endpoints to add and remove userNames

--- LoneLabWebApp/Controllers/GameController.cs
+++ LoneLabWebApp/Controllers/GameController.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using LoneLabWebApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LoneLabWebApp.Controllers
 {
     public class GameController : Controller
     {
+        private readonly ILoginService _loginService;
+        public GameController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(string userName)
+        {
+            _loginService.AddUserName(userName);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveUser(string userName)
+        {
+            _loginService.RemoveUserName(userName);
+            return Ok();
         }
     }
 }
---
+++ LoneLabWebApp/Services/Interfaces/ILoginService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LoneLabWebApp.Services.Interfaces
+{
+    public interface ILoginService
+    {
+        public void AddUserName(string userName);
+
+        public void RemoveUserName(string userName);
+
+        public List<string> GetUserNames();
+    }
+}
